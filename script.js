152ecf23502e5cf5d3541b2164a8e5a812fa90f6
ValentinaRippe <lauravalentina200102@gmail.com>
2021-12-20 22:23:49 -0500

    CSS, HTML and JavaSript changes, hamburger menu change, testimonials section, form section and footer, responsive and animations.

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 
 
 //Menu scroll
-let prevScrollpos = window.pageYOffset;
+let prev2Scrollpos = window.pageYOffset;
 window.onscroll = function() {
 let currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-document.getElementById("menu").style.top = "0";
+if (prev2Scrollpos > currentScrollPos) {
+document.getElementById("fixed").style.top = "0";
 }
  else {
-document.getElementById("menu").style.top = "-700px";
+document.getElementById("fixed").style.top = "-700px";
 }
-prevScrollpos = currentScrollPos;
+prev2Scrollpos = currentScrollPos;
 }
-//Animation proyect
-let removeStyleButton = document.getElementById("removeStyleButton")
-let blurLanding = document.getElementById("blurLanding")
+//filter blur proyect
+ document.getElementById("proyect1").onmouseover = function() {mouseOver()};
+ document.getElementById("proyect1").onmouseout = function() {mouseOut()};
 
-removeStyleButton.addEventListener('click', function(){
-    if(blurLanding.classList.contains('style')) {
-        blurLanding.classList.add('style');
-    } else{
-        blurLanding.classList.remove('style');
-    }
-})
+function mouseOver() {
+    document.getElementById("blurLanding").style.filter = "blur(0px)";
+}
+function mouseOut() {
+  document.getElementById("blurLanding").style.filter = "blur(5px)";
+}
+document.getElementById("proyect2").onmouseover = function() {mouseOver2()};
+document.getElementById("proyect2").onmouseout = function() {mouseOut2()};
 
+function mouseOver2() {
+   document.getElementById("blur2").style.filter = "blur(0px)";
+}
+function mouseOut2() {
+ document.getElementById("blur2").style.filter = "blur(5px)";
+}
+document.getElementById("proyect3").onmouseover = function() {mouseOver3()};
+document.getElementById("proyect3").onmouseout = function() {mouseOut3()};
+
+function mouseOver3() {
+   document.getElementById("blur3").style.filter = "blur(0px)";
+}
+function mouseOut3() {
+ document.getElementById("blur3").style.filter = "blur(5px)";
+}
 //Form submit
 const $form = document.querySelector('#form')
 const $buttonMailto = document.querySelector('#emailSubmit')
@@ -33,8 +49,6 @@ $form.addEventListener('submit', handleSubmit)
 function handleSubmit(event) {
   event.preventDefault()
   const form = new FormData(this)
-  emailSubmit.setAttribute('href', `mailto:lauravalentina200102@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
-  emailSubmit.click()
-  $buttonMailto.setAttribute('href', `mailto:lauravalentina200102@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.setAttribute('href', `mailto:lauravalentina200102@gmail.com?subject= nombre ${form.get('name')}  correo ${form.get('email')}&body= ${form.get('message')}`)
   $buttonMailto.click()
 }
